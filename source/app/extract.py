182a9ef005b538c9692c4c6da3b98ee200f0e5f3
greg-gg-smith <greg22smith@gmail.com>
2022-02-16 21:51:04 +0000

    uuid gone, only hash remains

--- source/app/extract.py
+++ source/app/extract.py
@@ -6,8 +6,10 @@ def extract(filename):
     field_names = ['date_time', 'store', 'customer', 'items', 'total_spend', 'payment_method', 'card_num']
     with open(filename, 'r') as csv_file:
         reader = csv.DictReader(csv_file, fieldnames = field_names)
+        id = 1
         for row in reader:
+            row['order_id'] = id
+            id+=1
             orders.append(row)
-    return orders
-
+        return orders
 
