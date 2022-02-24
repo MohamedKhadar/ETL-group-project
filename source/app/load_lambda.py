6dde62bbed84f21ede8c1a304bf41c127f023f1f
MohamedK <mohamedk2196@gmail.com>
2022-02-24 17:48:46 +0000

    working SQS Queue version

--- source/app/load_lambda.py
+++ source/app/load_lambda.py
@@ -17,22 +17,35 @@ LOGGER.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
     LOGGER.info(event)
-    filepath = '/tmp/somefile.csv'
-    s3_event = event['Records'][0]['s3']
-    bucket_name = s3_event['bucket']['name']
-    object_name = s3_event['object']['key']
-    LOGGER.info(f'triggered by file: {object_name} in bucket: {bucket_name}')
+    filepath = '/tmp/output.csv'
+    event_body = json.loads(event['Records'][0]['body'])
+    LOGGER.info(event_body)
+    s3_bucket = event_body['bucket_name']
+    s3_object_key = event_body['bucket_key']
+    data_type = event_body['data_type']
+    LOGGER.info(f'triggered by file: {s3_object_key} in bucket: {s3_bucket}')
     s3 = boto3.client('s3')
-    s3.download_file(bucket_name, object_name, filepath)
+    s3.download_file(s3_bucket, s3_object_key, filepath)
     
-    orders = extract.extract_orders(filepath)
-    products = extract.extract_products(filepath)
-    order_products = extract.extract_order_products(filepath)
-    
-    final_data = orders + products + order_products
     
     creds = db.get_ssm_parameters_under_path('/team3/redshift')
-    for x in final_data:
-        LOGGER.info('inserting order into database')
-        db.insert_order(creds, x)
-    
+    if data_type == 'orders':
+        orders = extract.extract_orders(filepath)
+        LOGGER.info(f'Extracted orders data first row {orders[0]}')
+        for x in orders:
+            LOGGER.info('inserting order into order table')
+            db.insert_order(creds, x)
+    elif data_type == 'products':
+        products = extract.extract_products(filepath)
+        LOGGER.info(f'Extracted products data first row {products[0]}')
+        for y in products:
+            LOGGER.info('inserting products into products table')
+            db.insert_product(creds, y)
+    elif data_type == 'order_products':
+        order_products = extract.extract_order_products(filepath)
+        LOGGER.info(f'Extracted order_products data first row {order_products[0]}')
+        for z in order_products:
+            LOGGER.info('inserting order_products into order_products table')
+            db.insert_order_product(creds, z)
+    else:
+        print(f"invalid data type {data_type}!")
